c90a9121150f00aa6918d6daa5ff9238543c717a
gogs <gogs@gogs.com>
2024-11-07 20:05:18 +0800

    feat(components): 增加悬浮显示功能并优化组件属性
    - 添加 float 属性以支持悬浮显示
    - 增加 type、icon、square 等属性扩展自定义选项
    - 优化组件样式和布局
    - 更新示例以展示新功能

--- play/src/next-backtop.vue
+++ play/src/next-backtop.vue
@@ -3,10 +3,11 @@
   <!-- <el-config-provider namespace="ep"> -->
   <div class="play-container">
     hello next-backtop
+
     <el-next-backtop
       target=".play-container"
       :right="100"
-      :bottom="200"
+      :bottom="300"
       :visibility-height="300"
     >
       <div
@@ -25,7 +26,16 @@
       <!-- <template #menu> <div>up3</div> </template> -->
     </el-next-backtop>
 
-    <!-- <el-backtop :bottom="100">
+    <el-next-backtop
+      float
+      square
+      type="success"
+      icon="Check"
+      :right="100"
+      :bottom="200"
+    />
+
+    <el-backtop :bottom="100">
       <div
         style="
           height: 100%;
@@ -40,13 +50,41 @@
         UP
       </div>
     </el-backtop>
-    <el-backtop :right="100" :bottom="100" /> -->
+    <el-backtop :right="100" :bottom="100" />
+  </div>
+
+  <div style="height: 400px">
+    <el-button @mouseenter="show = true" @mouseleave="show = false"
+      >hover Me</el-button
+    >
+
+    <div style="margin-top: 20px">
+      <transition name="el-zoom-in-bottom">
+        <div
+          v-show="show"
+          style="
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100px;
+            gap: 10px;
+          "
+        >
+          <el-button type="primary" :icon="Edit" circle />
+          <el-button type="success" :icon="Check" circle />
+          <el-button type="info" :icon="Message" circle />
+        </div>
+      </transition>
+    </div>
   </div>
   <!-- </el-config-provider> -->
 </template>
 
 <script setup lang="ts">
 // code here
+import { computed, defineEmits, defineProps, onMounted, ref, watch } from 'vue'
+const show = ref(false)
 </script>
 
 <style lang="scss">
@@ -66,6 +104,19 @@ body {
       display: flex;
       align-items: center;
       justify-content: center;
+    }
+
+    .transition-box {
+      margin-bottom: 10px;
+      width: 200px;
+      height: 100px;
+      border-radius: 4px;
+      background-color: #409eff;
+      text-align: center;
+      color: #fff;
+      padding: 40px 20px;
+      box-sizing: border-box;
+      margin-right: 20px;
     }
   }
 }
