7f1705efd0ee687d8fc8dae5acb60a245fd69eac
gogs <gogs@gogs.com>
2024-11-21 18:17:35 +0800

    feat(components): 添加 ElNextListItem 组件并完善 ElNextList 功能
    - 新增 ElNextListItem 组件，用于在 ElNextList 中展示列表项
    - 在 ElNextList 中添加 header 和 footer 插槽支持
    - 增加 size、split 和 stripe 属性，丰富列表展示效果
    - 更新样式文件，引入新的 next-list 样式
    - 修改 component.ts，注册 ElNextListItem 组件

--- packages/components/next-list/index.ts
+++ packages/components/next-list/index.ts
@@ -1,7 +1,17 @@
-import { withInstall } from '@element-plus/utils'
-import NextList from './src/next-list.vue'
+import { withInstall, withNoopInstall } from '@element-plus/utils'
 
-export const ElNextList = withInstall(NextList)
+import NextList from './src/next-list.vue'
+import NextListItem from './src/next-list-item.vue'
+import type { SFCWithInstall } from '@element-plus/utils'
+
+export const ElNextList: SFCWithInstall<typeof NextList> & {
+  NextListItem: typeof NextListItem
+} = withInstall(NextList, {
+  NextListItem,
+})
 export default ElNextList
+export const ElNextListItem: SFCWithInstall<typeof NextListItem> =
+  withNoopInstall(NextListItem)
 
 export * from './src/next-list'
+export * from './src/next-list-item'
--- packages/element-plus/component.ts
+++ packages/element-plus/component.ts
@@ -108,7 +108,7 @@ import { ElAnchor, ElAnchorLink } from '@element-plus/components/anchor'
 import { ElSegmented } from '@element-plus/components/segmented'
 import { ElMention } from '@element-plus/components/mention'
 
-import { ElNextList } from '@element-plus/components/next-list'
+import { ElNextList, ElNextListItem } from '@element-plus/components/next-list'
 
 import type { Plugin } from 'vue'
 
@@ -220,4 +220,5 @@ export default [
   ElMention,
 
   ElNextList,
+  ElNextListItem,
 ] as Plugin[]
